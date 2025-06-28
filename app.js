85b73d08f3f0bbff0f4d2378c3db0beafe9d4703
Nicashot <nicachot@gmail.com>
2025-06-27 19:31:31 -0500

    app use

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../views')));
 
 app.use('/api', authRoutes);
-app.use('/api/cliente', clienteRoutes);
+//app.use('/api/cliente', clienteRoutes);
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../frontend/views/index.html'));
